f91b25adf117fd2509089a32594c8d7d76721132
nihao <jackal186@163.com>
2019-12-29 21:20:21 +0800

    update 12.29

--- assets/views.py
+++ assets/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+
+from django.shortcuts import HttpResponse
+
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def report(request):
+    if request.method == "POST":
+        asset_data = request.POST.get('asset_data')
+        print(asset_data)
+        return HttpResponse("成功收到数据！")
